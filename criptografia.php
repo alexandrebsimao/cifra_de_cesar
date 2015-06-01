0a4b3807f680d4aa1995f70b03f6617272b9cac3
Alexandre A. B. Simao <alexandre.b.simao@gmail.com>
2015-06-01 14:43:49 -0300

    Validação de caracteres inexistentes

--- criptografia.php
+++ criptografia.php
@@ -1,9 +1,40 @@
 <?php
-	$key = 5;
-	$phrase = 'TESTE';
+	/*		
+		*** Cifrador de César ***
+		Algoritmo tem como objetivo mover uma letra 
+		em um número de casas determinado pela chave
+	*/
+
+	$key = 3; // Chave definida 
+	$phrase = 'FRASE AQUI';
 
 	$alpha = array(
-		'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+		1  => 'A',
+		2  => 'B',
+		3  => 'C',
+		4  => 'D',
+		5  => 'E',
+		6  => 'F',
+		7  => 'G',
+		8  => 'H',
+		9  => 'I',
+		10 => 'J',
+		11 => 'K',
+		12 => 'L',
+		13 => 'M',
+		14 => 'N',
+		15 => 'O',
+		16 => 'P',
+		17 => 'Q',
+		18 => 'R',
+		19 => 'S',
+		20 => 'T',
+		21 => 'U',
+		22 => 'V',
+		23 => 'W',
+		24 => 'X',
+		25 => 'Y',
+		26 => 'Z'
 		);
 
 	$cont = count($alpha) - 1;
@@ -13,12 +44,17 @@
 	$crypt = '';
 	foreach ($phrase as $v) {
 		$k_alpha = array_search($v, $alpha);
-		$k_alpha = $k_alpha + $key;
 
-		if( $k_alpha >= 25 ){
-			$k_alpha = $k_alpha - $cont;
+		if( $k_alpha ){
+			$k_alpha = $k_alpha + $key;
+
+			if( $k_alpha >= 25 ){
+				$k_alpha = $k_alpha - $cont;
+			}
+			$crypt .= $alpha[$k_alpha];
+		}else{
+			$crypt .= $v;
 		}
-		$crypt .= $alpha[$k_alpha];
 		// var_dump($k_alpha);
 	}
 
@@ -30,13 +66,18 @@
 	$decrypt = '';
 	foreach ($phrase as $v) {
 		$k_alpha = array_search($v, $alpha);
-		$k_alpha = $k_alpha - $key;
 
-		if( $k_alpha < 0 ){
-			$k_alpha = $k_alpha + $cont;
+		if( $k_alpha ){
+			$k_alpha = $k_alpha - $key;
+
+			if( $k_alpha < 0 ){
+				$k_alpha = $k_alpha + $cont;
+			}
+			$decrypt .= $alpha[$k_alpha];
+			// var_dump($k_alpha);
+		}else{
+			$decrypt .= $v;
 		}
-		$decrypt .= $alpha[$k_alpha];
-		// var_dump($k_alpha);
 	}
 
 	var_dump($decrypt);
